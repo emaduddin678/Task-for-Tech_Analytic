76bbc4e85d5860139470d6cf86b16ce5a7a6d65f
emaduddin678 <emaduddin678@gmail.com>
2024-05-15 12:52:00 +0600

    cart done and home ready almost

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,149 @@
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello </h1>
+    <main className="  p-24">
+      <div className="grid  gap-4 grid-cols-5 ">
+        <div className="col-span-3  rounded-lg bg-gray-200 ">
+          <div className="filterBox">
+            <div className="flex justify-between items-center">
+              <h1 className="text-[#282828] font-semibold text-3xl">
+                Our All Products
+              </h1>
+              <div className="flex justify-between items-center mr-2 gap-3">
+                <Image
+                  src={"/grid.svg"}
+                  width={25}
+                  height={25}
+                  alt={"This is grid icon."}
+                />
+                <Image
+                  src={"/list.svg"}
+                  width={25}
+                  height={25}
+                  alt={"This is list icon."}
+                />
+              </div>
+            </div>
+            <fieldset className="w-full space-y-1 text-gray-100">
+              <label htmlFor="Search" className="hidden">
+                Search An Item...
+              </label>
+              <div className="relative">
+                <span className="absolute inset-y-0 left-0 flex items-center pl-2">
+                  <button
+                    type="button"
+                    title="search"
+                    className="p-1 focus:outline-none focus:ring"
+                  >
+                    <svg
+                      fill="#212529"
+                      viewBox="0 0 512 512"
+                      className="w-4 h-4 text-gray-100"
+                    >
+                      <path d="M479.6,399.716l-81.084-81.084-62.368-25.767A175.014,175.014,0,0,0,368,192c0-97.047-78.953-176-176-176S16,94.953,16,192,94.953,368,192,368a175.034,175.034,0,0,0,101.619-32.377l25.7,62.2L400.4,478.911a56,56,0,1,0,79.2-79.195ZM48,192c0-79.4,64.6-144,144-144s144,64.6,144,144S271.4,336,192,336,48,271.4,48,192ZM456.971,456.284a24.028,24.028,0,0,1-33.942,0l-76.572-76.572-23.894-57.835L380.4,345.771l76.573,76.572A24.028,24.028,0,0,1,456.971,456.284Z"></path>
+                    </svg>
+                  </button>
+                </span>
+                <input
+                  type="search"
+                  name="Search"
+                  placeholder="Search An Item..."
+                  className="w-full rounded-3xl border-black outline-black text-black  py-2 pl-10 text-sm   focus:outline-none   focus:border-default-400"
+                />
+              </div>
+            </fieldset>
+          </div>
+          <div className="cartContainer grid grid-cols-2 justify-between gap-3 lg:grid-cols-3">
+            {/* single cart  */}
+            <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow  ">
+              <a href="#">
+                <Image
+                  src={"/head.png"}
+                  width={300}
+                  height={200}
+                  className="!w-full"
+                  alt="cart image"
+                  layout="responsive"
+                  quality={100}
+                />
+              </a>
+              <div className="px-5 pb-5">
+                <a href="#">
+                  <h5 className="text-base font-semibold tracking-tight text-gray-900 ">
+                    Wireless Bluetooth Headset Single Ear ...
+                  </h5>
+                </a>
+                <div className="flex items-center  my-4">
+                  <div>
+                    <div className="flex items-center space-x-1 rtl:space-x-reverse">
+                      <svg
+                        className="w-4 h-4 text-yellow-300"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 22 20"
+                      >
+                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                      </svg>
+                      <svg
+                        className="w-4 h-4 text-yellow-300"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 22 20"
+                      >
+                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                      </svg>
+                      <svg
+                        className="w-4 h-4 text-yellow-300"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 22 20"
+                      >
+                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                      </svg>
+                      <svg
+                        className="w-4 h-4 text-yellow-300"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 22 20"
+                      >
+                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                      </svg>
+                      <svg
+                        className="w-4 h-4 text-yellow-300"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 22 20"
+                      >
+                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                      </svg>
+                    </div>
+                    <p className="text-gray-500 ">(121 Review)</p>
+                  </div>
+                </div>
+                <div className="flex items-center justify-between">
+                  <span className="text-xl font-bold text-[#F2415A] ">
+                    $599
+                  </span>
+                  <a
+                    href="#"
+                    className="text-white bg-[#212529] hover:bg-[#F2415A] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
+                  >
+                    Add to cart
+                  </a>
+                </div>
+              </div>
+            </div>
+            {/* single cart  */}
+          </div>
+        </div>
+        <div className="col-span-2 h-32 rounded-lg bg-gray-200"></div>
+      </div>
     </main>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,118 +1,68 @@
+"use client";
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
+  const pathname = usePathname();
+  console.log(pathname);
+
   return (
     <header className="bg-[#212529]">
-      <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-screen-xl px-4 py-3 sm:px-6 lg:px-8">
         <div className="flex h-16 items-center justify-between">
           <div className="md:flex md:items-center md:gap-12">
             <a className="block text-teal-600" href="#">
-              <span className="sr-only">Home</span>
-              
+              <Image
+                src="/logo.png"
+                width={180}
+                height={50}
+                alt="Picture of the author"
+              />
             </a>
           </div>
 
           <div className="hidden md:block">
             <nav aria-label="Global">
               <ul className="flex items-center gap-6 text-sm">
-                <li>
-                  <a
-                    className="text-gray-500 transition hover:text-gray-500/75"
-                    href="#"
+                <li className={` ${pathname === "/" ? "active" : ""}`}>
+                  <Link
+                    className="text-white transition hover:text-gray-500/75 font-semibold "
+                    href="/"
                   >
-                    {" "}
-                    About{" "}
-                  </a>
+                    Products
+                  </Link>
                 </li>
 
-                <li>
-                  <a
-                    className="text-gray-500 transition hover:text-gray-500/75"
-                    href="#"
+                <li className={`${pathname === "/cart" ? "active" : ""}`}>
+                  <Link
+                    className="text-white transition hover:text-gray-500/75 font-semibold"
+                    href="/cart"
                   >
-                    {" "}
-                    Careers{" "}
-                  </a>
-                </li>
-
-                <li>
-                  <a
-                    className="text-gray-500 transition hover:text-gray-500/75"
-                    href="#"
-                  >
-                    {" "}
-                    History{" "}
-                  </a>
-                </li>
-
-                <li>
-                  <a
-                    className="text-gray-500 transition hover:text-gray-500/75"
-                    href="#"
-                  >
-                    {" "}
-                    Services{" "}
-                  </a>
-                </li>
-
-                <li>
-                  <a
-                    className="text-gray-500 transition hover:text-gray-500/75"
-                    href="#"
-                  >
-                    {" "}
-                    Projects{" "}
-                  </a>
-                </li>
-
-                <li>
-                  <a
-                    className="text-gray-500 transition hover:text-gray-500/75"
-                    href="#"
-                  >
-                    {" "}
-                    Blog{" "}
-                  </a>
+                    Cart
+                  </Link>
                 </li>
               </ul>
             </nav>
           </div>
 
           <div className="flex items-center gap-4">
-            <div className="sm:flex sm:gap-4">
-              <a
-                className="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow"
-                href="#"
-              >
-                Login
-              </a>
+            <a
+              className=" lg:block hidden rounded-md bg-[#525CEB] px-5 py-2.5 text-sm font-medium text-white shadow"
+              href="#"
+            >
+              Login
+            </a>
 
-              <div className="hidden sm:flex">
-                <a
-                  className="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600"
-                  href="#"
-                >
-                  Register
-                </a>
-              </div>
-            </div>
-
-            <div className="block md:hidden">
-              <button className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                </svg>
+            <div className="block lg:hidden">
+              <button className="rounded  p-2 text-gray-600 transition hover:text-gray-600/75">
+                <Image
+                  src="/bar.svg"
+                  width={24}
+                  height={24}
+                  alt="Picture of the author"
+                />
               </button>
             </div>
           </div>
